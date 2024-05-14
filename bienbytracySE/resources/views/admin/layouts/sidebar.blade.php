9830d4d38ec9694ad9bb7193a794ae086634059e
Alliana Borcena <allianaborcena@Allianas-MacBook-Pro.local>
2024-05-14 22:28:41 +0800

    Borcena()-> ProductCategory

--- bienbytracySE/resources/views/admin/layouts/sidebar.blade.php
+++ bienbytracySE/resources/views/admin/layouts/sidebar.blade.php
@@ -60,6 +60,13 @@
 
                 </ul>
             </li>
+
+            <li class = "dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class ="fas fa-columns"></i>
+                    <span>Manage Menu</span></a>
+                <ul class = "dropdown-menu">
+                    <li><a class="nav-link" href="{{ route('admin.category.index') }}">Product Categories</a></li>
+
             {{-- <li><a class="nav-link" href="{{ route('admin.slider.index') }}"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
             {{-- <ul class= --}}
         </ul>
