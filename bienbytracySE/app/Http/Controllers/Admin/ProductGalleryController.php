01521aec9eb414315afb593786c83a19b74814bc
Alliana Borcena <allianaborcena@Allianas-MacBook-Pro.local>
2024-05-16 23:39:26 +0800

    Borcena()-> AdminProductGallery

--- bienbytracySE/app/Http/Controllers/Admin/ProductGalleryController.php
+++ bienbytracySE/app/Http/Controllers/Admin/ProductGalleryController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductGallery;
 use App\Traits\FileUploadTrait;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class ProductGalleryController extends Controller
@@ -13,11 +17,17 @@ class ProductGalleryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() : View
-    {
-        return view ('admin.product.gallery.index');
-    }
+    public function index(string $productId): View
+{
+    $images = ProductGallery::where('product_id',$productId)->get();
+    // Retrieve the product data
+    $product = Product::findOrFail($productId); // Assuming you have a Product model
 
+    // Retrieve the gallery images associated with the product
+    $images = ProductGallery::where('product_id', $productId)->get();
+
+    return view('admin.product.gallery.index', compact('product', 'images'));
+}
     /**
      * Show the form for creating a new resource.
      */
@@ -30,14 +40,24 @@ class ProductGalleryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request) : RedirectResponse
     {
         $request->validate([
-            'image' => ['required','image','max:3000']
+            'image' => ['required', 'image', 'max:3000'],
+            'product_id' => ['required', 'integer']
         ]);
 
         $imagePath = $this-> uploadImage($request, 'image');
-        
+
+        $gallery = new ProductGallery();
+        $gallery->product_id = $request->product_id;
+        $gallery->image = $imagePath;
+        $gallery->save();
+
+        toastr()->success('Created Successfully!');
+
+        return redirect()->back();
+
     }
 
     /**
@@ -69,6 +89,16 @@ class ProductGalleryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try{
+            $image = ProductGallery::findOrFail($id);
+            $this->removeImage($image->image);
+            $image->delete();
+
+        return response()->json(['status' => 'success', 'message' => 'Deleted Successfully!']);
+    } catch (\Exception $e) {
+        // If an exception occurs (e.g., user not found), return an error response
+        return response()->json(['status' => 'error', 'message' => 'something went wrong!']);
     }
 }
+}
+
